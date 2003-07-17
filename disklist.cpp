fcc987eb5f2cdbcd21f0ef827a2e5ab5ce1e8eb9
Dirk Mueller <mueller@kde.org>
2003-07-17 11:02:38 +0000

    compile fixes (gcc 3.4+)
    svn path=/branches/KDE_3_1_BRANCH/kdeutils/kdf/; revision=237565

--- disklist.cpp
+++ disklist.cpp
@@ -84,7 +84,7 @@ if (NO_FS_TYPE)
        loadSettings();
        //readFSTAB();
        //readDF();
-};
+}
 
 
 /***************************************************************************
@@ -92,7 +92,7 @@ if (NO_FS_TYPE)
 **/
 DiskList::~DiskList()
 {
-};
+}
 
 /**
 Updated need to be disabled sometimes to avoid pulling the DiskEntry out from the popupmenu handler
@@ -160,7 +160,7 @@ QString rc;
         if (s[i] == '\\') {
             i++;
             switch(s[i]) {
-            case '\\':  // slash \\   
+            case '\\':
                rc += '\\';
                break;
             case '0': // octal 0nn
